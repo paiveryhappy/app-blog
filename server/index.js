9b330fc44e4f6fd18289c65071911960f2466914
Jiran <paiveryhappy@gmail.com>
2024-04-30 20:56:56 +0700

    feat: add home component and fetch blog data

--- server/index.js
+++ server/index.js
@@ -1,55 +1,27 @@
-const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
+import logging from "./middlewares/logging.js";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import blogData from "./utils/blogData.js";
 
 const port = 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logging);
 
 app.listen(port, () => {
   console.log(`server is running at ${port}`);
 });
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "bambooblog",
-});
-
-db.connect(function (err) {
-  if (err) {
-    console.error("cannot connect to database", err);
-  } else {
-    console.log("connect successful");
-  }
-});
-
 app.get("/", (req, res) => {
-  return res.json("Hello world");
+  return res.json("Hello JC Article");
 });
 
-app.post("/", async (req, res) => {
-  //let dataFromClient = [req.body.username, req.body.email, req.body.password];
-  let { username, email, password } = req.body;
-  const hashPassword = await bcrypt.hash(password, 10);
-  let userData = {
-    username: username,
-    email: email,
-    password: hashPassword,
-  };
-  const sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
-  db.query(sql, [username, email, hashPassword], (err, result) => {
-    if (err) {
-      res.status(500).send("เกิดข้อผิดพลาดในการ Insert ข้อมูล");
-      console.error("เกิดข้อผิดพลาดในการ Insert ข้อมูล: ", err);
-      return;
-    }
-    res.status(200).send("ข้อมูลถูก Insert ลงในฐานข้อมูลแล้ว");
-    console.log("Insert data complete");
+app.get("/myblog", (req, res) => {
+  res.json({
+    data: blogData,
   });
 });
---
+++ server/utils/blogData.js
@@ -0,0 +1,60 @@
+let blogData = [
+  {
+    id: 1,
+    imageContent:
+      "https://images.pexels.com/photos/3411134/pexels-photo-3411134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    topic: "Disney's motion priciple in designing interface animations",
+    create_at: {
+      date: 13,
+      month: "January",
+      year: 2020,
+    },
+    watch: 6500,
+    likes: 3400,
+    price: 34,
+  },
+  {
+    id: 2,
+    imageContent:
+      "https://images.pexels.com/photos/19435870/pexels-photo-19435870.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    topic: "How Airbnb drives users actions with their landing page design",
+    create_at: {
+      date: 16,
+      month: "March",
+      year: 2020,
+    },
+    watch: 5300,
+    likes: 3100,
+    price: 28,
+  },
+  {
+    id: 3,
+    imageContent:
+      "https://images.pexels.com/photos/936043/pexels-photo-936043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    topic: "It's time for Tech to Ask `Should We` Instead of `Could We`",
+    create_at: {
+      date: 1,
+      month: "April",
+      year: 2020,
+    },
+    watch: 3100,
+    likes: 2200,
+    price: 25,
+  },
+  {
+    id: 4,
+    imageContent:
+      "https://images.pexels.com/photos/927629/pexels-photo-927629.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    topic: "Facebook creates Radioactive Images, and Other A.I.",
+    create_at: {
+      date: 25,
+      month: "December",
+      year: 2020,
+    },
+    watch: 2800,
+    likes: 1700,
+    price: 18,
+  },
+];
+
+export default blogData;
